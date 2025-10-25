c191d4e5a38a636181e92515e2a9884e8b05584c
T-r-666 <1154730956@qq.com>
2025-10-25 19:21:37 +0800

    实现 PrimeList(N) 函数

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ def PrimeList(N):
     参数:    N - 正整数    
     返回:    str - 包含所有小于 N 的质数的字符串，空格分隔
     """
-    
+   import math
+
+def PrimeList(N):
+    primes = []
+    for i in range(2, N):
+        is_prime = True
+        sqrt_i = int(math.sqrt(i)) + 1
+        for j in range(2, sqrt_i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(str(i))
+    return ' '.join(primes) 
