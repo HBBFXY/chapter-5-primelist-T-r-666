914ba103b51660e67ac53c739d4f08acbae544be
T-r-666 <1154730956@qq.com>
2025-10-25 19:24:30 +0800

    完成实现 PrimeList(N) 函数的作业

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # 在此文件中实现 PrimeList 函数
-   import math
-
+import math
 def PrimeList(N):
     primes = []
     for i in range(2, N):
         is_prime = True
-        sqrt_i = int(math.sqrt(i)) + 1
-        for j in range(2, sqrt_i):
+        for j in range(2, int(math.sqrt(i)) + 1):
             if i % j == 0:
                 is_prime = False
                 break
         if is_prime:
             primes.append(str(i))
-    return ' '.join(primes) 
+    return ' '.join(primes)
